327d261883d45f179685797803acf5218db5d747
ubungmeister <ubungmeister@gmail.com>
2022-08-22 15:27:21 +0200

    Graph

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,47 +1,20 @@
 import React, {useState} from 'react';
 import {Graph} from "./Graph";
-import {
-    CartesianGrid,
-    Legend,
-    Line,
-    LineChart,
-    ResponsiveContainer,
-    Tooltip,
-    XAxis,
-    YAxis,
-} from 'recharts'
 
-import {handleMortgageDataChange, handleMortgageDataChangeGraph} from "./utils";
+import {handleMortgageDataChange} from "./utils";
 import {PaymentTable} from "./PaymentTable";
-import {PaymentGraph} from "./PaymentGraph";
 import {CalculatorInput} from "./CalculatorInput";
 
 export const Calculator = () => {
     const [amountBorrow, setStateAmount] = useState<number>(0)
     const [interest, setStateMortgage] = useState<number>(0)
     const [yearPay, setStateMonth] = useState<number>(0)
-    const formatDecimals = (item: number) => {
-        return Number(item.toFixed(2))
-    }
-
 
     //monthly annual interest
     const monthlyRate = ((interest / 100 / 12) * amountBorrow) / (1 - (Math.pow((1 + (interest / 100 / 12)),((0 - yearPay) * 12))))
     // data for table and graph
     const dataCalculateMortgage = handleMortgageDataChange(amountBorrow,interest,yearPay,monthlyRate)
-    const yearPayments = handleMortgageDataChange(amountBorrow,interest,yearPay,monthlyRate)
 
-    type DataCalculateMortgage = ReturnType<typeof handleMortgageDataChange>
-    // const Charts = (props: { calculatedMortgage: DataCalculateMortgage }) => {
-    //     const chartData = props.calculatedMortgage.map((item, index) => ({
-    //         xAxis: { index },
-    //         interestPaid: formatDecimals(item.interestPaid),
-    //         principalPaid: formatDecimals(item.principalRepaid),
-    //         remain: formatDecimals(item.principalRepaid),
-    //     }))
-
-
-        const [collapsed, setCollapsed] = useState(true)
 
     return (
         <div>
@@ -57,69 +30,6 @@ export const Calculator = () => {
                 <Graph calculatedMortgage={dataCalculateMortgage}/>
                 <PaymentTable monthlyPayments={dataCalculateMortgage}/>
             </div>
-            {/*<div >*/}
-            {/*    <button onClick={()=>{setCollapsed(!collapsed)}}>Show payment schedule*/}
-            {/*    </button>*/}
-            {/*    /!*{!collapsed && <PaymentTable monthlyPayments={calculateMortgage}/>}*!/*/}
-            {/*    /!*<PaymentGraph yearPayments={yearPayments} yearPay={yearPay}/>*!/*/}
-            {/*</div>*/}
-            {/*<Charts calculatedMortgage={dataCalculateMortgage}/>*/}
-            {/*<div>*/}
-            {/*    <div>*/}
-            {/*        <LineChart*/}
-            {/*            width={600}*/}
-            {/*            height={300}*/}
-            {/*            data={chartData}*/}
-            {/*            margin={{*/}
-            {/*                top: 15,*/}
-            {/*                right: 30,*/}
-            {/*                left: 10,*/}
-            {/*                bottom: 15,*/}
-            {/*            }}*/}
-            {/*        >*/}
-            {/*            <CartesianGrid stroke='#eee' strokeDasharray='3 3' />*/}
-            {/*            <XAxis dataKey='xAxis' />*/}
-            {/*            <YAxis />*/}
-            {/*            <Tooltip />*/}
-            {/*            <Legend />*/}
-            {/*            <Line*/}
-            {/*                type='monotone'*/}
-            {/*                dataKey='remain'*/}
-            {/*                activeDot={{ r: 8 }}*/}
-            {/*            />*/}
-            {/*        </LineChart>*/}
-            {/*        <LineChart*/}
-            {/*            width={600}*/}
-            {/*            height={300}*/}
-            {/*            data={chartData}*/}
-            {/*            margin={{*/}
-            {/*                top: 15,*/}
-            {/*                right: 30,*/}
-            {/*                left: 10,*/}
-            {/*                bottom: 15,*/}
-            {/*            }}*/}
-            {/*        >*/}
-            {/*            <CartesianGrid stroke='#eee' strokeDasharray='3 3' />*/}
-            {/*            <XAxis dataKey='xAxis' />*/}
-            {/*            <YAxis />*/}
-            {/*            <Tooltip />*/}
-            {/*            <Legend />*/}
-            {/*            <Line*/}
-            {/*                type='monotone'*/}
-            {/*                dataKey='interestPaid'*/}
-            {/*                strokeWidth={1}*/}
-            {/*                activeDot={{ r: 8 }}*/}
-            {/*            />*/}
-            {/*            <Line*/}
-            {/*                type='monotone'*/}
-            {/*                dataKey='principalPaid'*/}
-            {/*                strokeWidth={1}*/}
-            {/*                activeDot={{ r: 8 }}*/}
-            {/*            />*/}
-            {/*        </LineChart>*/}
-            {/*    </div>*/}
-            {/*    )*/}
-            {/*</div>*/}
         </div>
     );
 };
--- src/components/utils.tsx
+++ src/components/utils.tsx
@@ -12,16 +12,11 @@ export const handleMortgageDataChange=(amountBorrow:number,interest:number,yearP
         interestPaidToDate: 0,
         principalRepaid: 0,
         principalRepaidToDate: 0,
-        principalRepaidByYear:0,
-        interestRepaidByYear:0
     }];
     let outstandingBalance = amountBorrow;
     let interestPaidToDate = 0;
     let principalRepaidToDate = 0;
     yearPay = yearPay*12
-    let counter = 0
-    let principalRepaidByYear = 0
-    let interestRepaidByYear = 0
 
     //Loop each year of the mortgage term
     for(let i = 1; i <= yearPay; i++) {
@@ -33,8 +28,7 @@ export const handleMortgageDataChange=(amountBorrow:number,interest:number,yearP
         let monthInterestPaid = getMonthInterestPaid(interest,outstandingBalance)
          //accumulative monthly interest paid
         interestPaidToDate = interestPaidToDate + monthInterestPaid;
-        // acc by each Year interest
-        interestRepaidByYear = interestRepaidByYear + monthInterestPaid
+
          //monthly principal
         const getMonthPrincipalPaid = (monthlyRate:number,monthInterestPaid:number)=>{
             return monthlyRate - monthInterestPaid
@@ -42,8 +36,6 @@ export const handleMortgageDataChange=(amountBorrow:number,interest:number,yearP
         let monthPrincipalPaid = getMonthPrincipalPaid(monthlyRate,monthInterestPaid)
          //accumulative monthly principal
         principalRepaidToDate = principalRepaidToDate + monthPrincipalPaid
-        // acc by each Year principal
-        principalRepaidByYear = principalRepaidByYear + monthPrincipalPaid
 
         //loan left to pay
         outstandingBalance = outstandingBalance -monthPrincipalPaid
@@ -60,71 +52,8 @@ export const handleMortgageDataChange=(amountBorrow:number,interest:number,yearP
             interestPaid: parseFloat(monthInterestPaid.toFixed(2)),
             interestPaidToDate: parseFloat(interestPaidToDate.toFixed(2)),
             principalRepaid: parseFloat(monthPrincipalPaid.toFixed(2)),
-            principalRepaidToDate: parseFloat(principalRepaidToDate.toFixed(2)),
-            principalRepaidByYear: principalRepaidByYear,
-            interestRepaidByYear:interestRepaidByYear
-        });
-        counter = counter +1
-        if(counter % 12 === 0){
-            principalRepaidByYear = 0
-            interestRepaidByYear = 0
-        }
-    }
-    return monthDataObject
-}
-
-
-
-
-
-export const handleMortgageDataChangeGraph = (amountBorrow:number,interest:number,yearPay:number,monthlyRate:number) => {
-
-    //Set initial values for loop to calculate yearly figures
-    let yearDataObject = [{
-        year: 0,
-        outstandingBalance: amountBorrow,
-        interestPaid: 0,
-        interestPaidToDate: 0,
-        principalRepaid: 0,
-        principalRepaidToDate: 0
-    }];
-    let outstandingBalance = amountBorrow;
-    let interestPaidToDate = 0;
-    let principalRepaidToDate = 0;
-
-    //Loop each year of the mortgage term
-    for(let i = 1; i <= yearPay; i++) {
-
-        let monthInterestPaid = 0;
-        let interestPaidMonthlyToYearlyIncrementer = 0;
-        let monthPrincipalPaid = 0;
-        let monthlyPrincipalRepaidToYearlyIncrementer = 0;
-
-        //loop each month of the year as interest is calculated monthly
-        for(let j = 0; j < 12; j++) {
-            monthInterestPaid = outstandingBalance * interest / 100 / 12;
-            interestPaidMonthlyToYearlyIncrementer = interestPaidMonthlyToYearlyIncrementer + monthInterestPaid;
-            monthPrincipalPaid = monthlyRate - monthInterestPaid;
-            monthlyPrincipalRepaidToYearlyIncrementer = monthlyPrincipalRepaidToYearlyIncrementer + monthPrincipalPaid;
-            outstandingBalance = outstandingBalance - monthPrincipalPaid;
-        }
-
-        interestPaidToDate = interestPaidToDate + interestPaidMonthlyToYearlyIncrementer;
-        principalRepaidToDate = principalRepaidToDate + monthlyPrincipalRepaidToYearlyIncrementer;
-
-        //There's always around £10 left at the end which forces the fraph to go into minus. This just rounds the last figure off at £0.00.
-        if(i === yearPay) {
-            outstandingBalance = 0;
-        }
-
-        yearDataObject.push({
-            year: i,
-            outstandingBalance: parseFloat(outstandingBalance.toFixed(2)),
-            interestPaid: parseFloat(interestPaidMonthlyToYearlyIncrementer.toFixed(2)),
-            interestPaidToDate: parseFloat(interestPaidToDate.toFixed(2)),
-            principalRepaid: parseFloat(monthlyPrincipalRepaidToYearlyIncrementer.toFixed(2)),
             principalRepaidToDate: parseFloat(principalRepaidToDate.toFixed(2))
         });
     }
-    return yearDataObject
+    return monthDataObject
 }
